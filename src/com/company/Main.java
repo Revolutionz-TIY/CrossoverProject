c3c61f0989a62c41485393f7c9315aa38c3f8af8
Erik Jakubowski <erikjakubowski@eriks-air.apollosrc.root>
2016-12-12 10:59:56 -0600

    we rock

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //more classes
+        //spark
+        //jodd-json
+        //varibles
+        
+
+
+
+    }
+}
