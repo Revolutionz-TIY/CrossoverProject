73c931ca38b24a4d2aa8833d500db8ea31db707a
Jenessa <jenessaw31@gmail.com>
2016-12-12 16:38:49 -0600

    added designs and some images

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor (props) {
     super (props)
+    this.state = {
+      results: []
+    }
   }
   componentDidMount () {
     this.getApi();
   }
   getApi () {
-
+    axios.get('http://localhost:3000/api/hello')
+    .then((response) => {
+      console.log(response);
+      let results = response.slice(0);
+      console.log(results);
+      this.setState ({
+        results: results,
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
   render() {
     return (
@@ -20,8 +35,16 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.results.map((result, index) => {
+            console.log(result);
+            return (
+              <div>
+                {result}
+              </div>
+            )
+          })}
         </p>
+
       </div>
     );
   }
