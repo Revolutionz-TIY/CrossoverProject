de61da517085d7bb87fae2ebf7e2bf6789caec3e
erikjakubowski <erik.jakubowski90@gmail.com>
2016-12-13 11:47:37 -0600

    new jar

---
+++ src/com/company/Tax.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by erikjakubowski on 12/13/16.
+ */
+public class Tax {
+    String zip;
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Tax(String zip) {
+        this.zip = zip;
+    }
+}
