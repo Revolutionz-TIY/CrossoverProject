f21e1c81058205badd523038d017798419376e0a
Bill Gottsch <billgottsch@gmail.com>
2016-12-13 11:01:15 -0600

    added info to page

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import api from './Api.js'
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor (props) {
@@ -29,20 +29,26 @@ class App extends Component {
       .catch(function (error) {
         console.log(error);
       });
+    }
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="StorePageContent">
+        {this.state.results.map((result, index) => {
+          console.log(result);
+          return (
+            <div className="storeInformation">
+              <ul className="StorePageList">
+                <li>{result.name}</li>
+                <li>{result.description}</li>
+                <li>{result.price}</li>
+                <li>{result.type}</li>
+              </ul>
+            </div>
+          )
+        })}
       </div>
-    );
+    )
   }
 }
-
 export default App;
