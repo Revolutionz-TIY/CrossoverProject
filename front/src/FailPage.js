68e9789cfad8fe2881376d545e55957872534ff8
Bill Gottsch <billgottsch@gmail.com>
2016-12-14 15:25:53 -0600

    added 404 page and styling

--- front/src/FailPage.js
+++ front/src/FailPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './App.css';
+import { Link } from 'react-router';
 
 export default class FailPage extends Component {
   constructor(props) {
@@ -8,9 +10,24 @@ export default class FailPage extends Component {
   }
   render() {
     return (
-      <div>
-        FailPage
+      <div className="FailPage">
+        <div className="fail-text">
+          <h2> Uh oh...</h2>
+            Uhh... that page never returned from it's trip.
+            That won't happen to you...we promise!
+          <br />
+            Just come back to us at the
+
+          <span className="fail-text">
+            <Link to={'/'}  className="fail-reroute"> Homepage </Link>
+          </span>
+          and we'll get you going again.
+        </div>
+        <div className="fail-text-title">
+          404!
       </div>
+
+    </div>
     );
   }
 }
